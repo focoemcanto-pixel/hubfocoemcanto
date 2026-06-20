e7ffe8692bd4049adbd802790d97efb3bf92b670
focoemcanto-pixel <focoemcanto@gmail.com>
2026-06-20 14:05:26 -0300

    feat: add Google Drive OAuth connect route

---
+++ app/admin/google/connect/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import { driveRedirectUri } from '@/lib/google/drive-utils';
+
+export async function GET() {
+  const params = new URLSearchParams();
+  params.set('client_id', process.env.GOOGLE_CLIENT_ID || '');
+  params.set('redirect_uri', driveRedirectUri());
+  params.set('response_type', 'code');
+  params.set('access_type', 'offline');
+  params.set('prompt', 'consent');
+  params.set('include_granted_scopes', 'true');
+  params.set('scope', 'https://www.googleapis.com/auth/drive.readonly');
+
+  const url = 'https://accounts.google.com/o/oauth2/v2/auth?' + params.toString();
+  return NextResponse.redirect(url);
+}
