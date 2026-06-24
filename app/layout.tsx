94d21158a8c152c420191c793033af418faf79a8
focoemcanto-pixel <focoemcanto@gmail.com>
2026-06-24 18:28:11 -0300

    Importa ajustes mobile da captura vocal

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ import './community-publish-menu.css';
 import './duet-publish-progress.css';
 import './duet-refined-flow.css';
 import './profile-icon-alignment.css';
+import './vocal-capture-mobile-fix.css';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return <html lang="pt-BR"><body>{props.children}</body></html>;
