e6b68a5e45724d57e92cd020c65f9428f39b0513
focoemcanto-pixel <focoemcanto@gmail.com>
2026-06-24 08:39:12 -0300

    Add vocal profile component placeholder

---
+++ components/vocal-profile-assessment.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export function VocalProfileAssessment() {
+  return (
+    <main style={{ padding: 24, color: '#fff' }}>
+      <h1>Perfil Vocal</h1>
+      <p>MVP inicial do mapa vocal em construção.</p>
+    </main>
+  );
+}
